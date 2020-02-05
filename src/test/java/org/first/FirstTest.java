7425068901c1d4e2e080c990c6ddda7f6a4f4b87
prateekshah <shah.prateek1991@gmail.com>
2020-02-05 12:27:18 +0530

    First failing test

---
+++ src/test/java/org/first/FirstTest.java
@@ -0,0 +1,12 @@
+package org.first;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(false);
+    }
+}
